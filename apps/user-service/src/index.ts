7727f1643c13c26416c703577eb7ae0c00f5345c
10xdevian <vikram.kr.gupta01@gmail.com>
2026-01-04 01:33:45 +0530

    test formatting

--- apps/user-service/src/index.ts
+++ apps/user-service/src/index.ts
@@ -1,10 +1,10 @@
 // apps/user-service/src/lint-test.ts
 
-// MISTAKE 1: Unused variable (Should trigger a warning)wrht
+// MISTAKE 1: Unused variable (Should trigger a warning)wrhtuer
 const unusedVar = "I am not used";
 
-// MISTAKE 2: Using 'var' instead of 'const/let' (Should trigger a warning)ieur
+// MISTAKE 2: Using 'var' instead of 'const/let' (Should trigger a warning)ieuruewr
 const oldStyle = 123;
 
-// OVERRIDE CHECK: Console log (Should pass silently because you set "no-console": "off")
+// OVERRIDE CHECK: Console log (Should pass silently because you set "no-console": "off")rwutu
 console.log("This should be allowed!");
